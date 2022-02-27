d0e2ec4d9216b8713600da40946505356167cb59
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2022-02-28 00:05:07 +0300

    Ну вроде все

--- LR-1/Main_elem_col.py
+++ LR-1/Main_elem_col.py
@@ -1,40 +1,36 @@
-import numpy as np
+from Gauss import triangle_form
 
 
-def gauss_pivot_func(matrix_a, b):
-    for k in range(matrix_a.shape[0] - 1):
-        # поиск строки с максимальным элементом
-        max_elem = 0
-        strng = 0
-        for i in range(k, matrix_a.shape[0]):
-            if abs(matrix_a[i, k]) > abs(max_elem):
-                max_elem = matrix_a[i, k]
-                strng = i
-        # меняем местами строки квадратной матрицы
-        change = np.repeat(matrix_a[k], 1)
-        matrix_a[k], matrix_a[strng] = matrix_a[strng], change
-        # меняем местами элементы вектора-столбца
-        change = np.repeat(b[k], 1)
-        b[k], b[strng] = b[strng], change
-        # делим полученную строку на max_elem
-        matrix_a[k] = matrix_a[k] / max_elem
-        b[k] = b[k] / max_elem
-        # домножаем строку на коэффициенты и вычитаем ее из остальных строк
-        for i in range(k + 1, matrix_a.shape[0]):
-            factor = matrix_a[i, k]
-            matrix_a[i] = matrix_a[i] - matrix_a[k] * factor
-            b[i] = b[i] - b[k] * factor
+def gauss_main_col_func(matrix_a, b):
+    for i in range(0, matrix_a.shape[0]):
 
-    # находим аргументы уравнений
-    x = [b[b.shape[0] - 1] / (matrix_a[matrix_a.shape[0] - 1, matrix_a.shape[0] - 1])]
-    for i in range(matrix_a.shape[0] - 2, -1, -1):
-        n = b[i]
-        for j in range(len(x)):
-            n = n - x[j] * matrix_a[i, matrix_a.shape[0] - 1 - j]
-        x.append(n)
+        # поиск главного по столбцу элемента
+        max_elem = matrix_a[i][i]
+        max_index = i
+        for j in range(i + 1, matrix_a.shape[0]):
+            if abs(max_elem) < abs(matrix_a[j][i]):
+                max_elem = matrix_a[j][i]
+                max_index = j
 
-    # переворачиваем значения в списке
-    x_norm = []
-    for i in reversed(x):
-        x_norm.append(i)
-    return x_norm, matrix_a, b
+        if i != max_index:
+            b[i], b[max_index] = b[max_index], b[i]
+            for j in range(i, matrix_a.shape[0]):
+                matrix_a[i][j], matrix_a[max_index][j] = matrix_a[max_index][j], matrix_a[i][j]
+
+        # треугольный вид
+        trng_res = triangle_form(matrix_a, b, i)
+        matrix_a = trng_res[0]
+        b = trng_res[1]
+
+    # ищем решения
+    x = [0 for _ in range(matrix_a.shape[0])]
+    for i in range(matrix_a.shape[0] - 1, -1, -1):
+        for j in range(i, matrix_a.shape[0]):
+            if j == matrix_a.shape[0] - 1:
+                break
+            else:
+                b[i] -= matrix_a[i][j + 1] * x[j + 1]
+        if matrix_a[i][i] != 0:
+            x[i] = b[i] / matrix_a[i][i]
+
+    return x, matrix_a, b
--- LR-1/main.py
+++ LR-1/main.py
@@ -1,40 +1,54 @@
 import numpy as np
 
 from Gauss import gauss_func
-from Main_elem_col import gauss_pivot_func
-
-D = np.array([[2.33, 0.81, 0.67, 0.92, -0.53],
-              [-0.53, 2.33, 0.81, 0.67, 0.92],
-              [0.92, -0.53, 2.33, 0.81, 0.67],
-              [0.67, 0.92, -0.53, 2.33, 0.81],
-              [0.81, 0.67, 0.92, -0.53, 2.33]])
-
-C = np.array([[0.2, 0, 0.2, 0, 0],
-              [0, 0.2, 0, 0.2, 0],
-              [0.2, 0, 0.2, 0, 0.2],
-              [0, 0.2, 0, 0.2, 0],
-              [0, 0, 0.2, 0, 0.2]])
-
-b = np.array([4.2 for i in range(5)])
+from Main_elem_col import gauss_main_col_func
+from Main_elem_full import gauss_main_full_func
 
 
-def output(result):
-    result_x = result[0]
-    result_b = result[2]
-    result_matrix_a = np.c_[result[1], result_b.T]
-    print(f"\nПолученная матрица:\n {result_matrix_a}")
+def val():
+    d = np.array([[2.33, 0.81, 0.67, 0.92, -0.53],
+                  [-0.53, 2.33, 0.81, 0.67, 0.92],
+                  [0.92, -0.53, 2.33, 0.81, 0.67],
+                  [0.67, 0.92, -0.53, 2.33, 0.81],
+                  [0.81, 0.67, 0.92, -0.53, 2.33]])
+
+    c = np.array([[0.2, 0, 0.2, 0, 0],
+                  [0, 0.2, 0, 0.2, 0],
+                  [0.2, 0, 0.2, 0, 0.2],
+                  [0, 0.2, 0, 0.2, 0],
+                  [0, 0, 0.2, 0, 0.2]])
+
+    b = np.array([4.2 for _ in range(5)])
+    a = 4 * c + d
+    return a, b
+
+
+def output(result_full):
+    np.set_printoptions(formatter={'float': '{: 0.4f}'.format})
+    result_x = result_full[0]
+    result_matrix_a = np.c_[result_full[1], result_full[2].T]
+    print(f"Полученная матрица:\n {result_matrix_a}")
     print("\nРезультат:")
-    for i in range(A.shape[0]):
-        print(f"x[{i + 1}] =" "%.4f" % result_x[i])
+    for i in range(val()[0].shape[0]):
+        print(f"x_{i + 1} = " "%.4f" % result_x[i])
 
 
 if __name__ == '__main__':
-    A = 4 * C + D
-    print("Метод Гаусса:")
-    result = gauss_func(A, b)
+
+    print("\nМетод Гаусса:\n-------------")
+    result = gauss_func(val()[0], val()[1])
     if result is not None:
         output(result)
+    print("======================================================================================")
 
-    print("Метод Гаусса с выбором главного элемента по столбцу:")
-    result = gauss_pivot_func(A, b)
+    print("\n\nМетод Гаусса с выбором главного элемента по столбцу:\n-----------------------------"
+          "-----------------------")
+    result = gauss_main_col_func(val()[0], val()[1])
     output(result)
+    print("======================================================================================")
+
+    print("\n\nМетод Гаусса с выбором главного элемента по всей матрице:\n------------------------"
+          "---------------------------------")
+    result = gauss_main_full_func(val()[0], val()[1])
+    output(result)
+    print("======================================================================================")
