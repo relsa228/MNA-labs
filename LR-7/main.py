5c0bce2548f4cfc58050cd7646acb52286a6b821
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2022-05-02 04:06:42 +0300

    LR-7

--- LR-7/main.py
+++ LR-7/main.py
@@ -1,55 +1,27 @@
-#! /usr/bin/python
-# -*- coding: utf-8 -*-
-u"""
-Cubic Spline library
-author Atsushi Sakai
-license: MIT
-"""
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
 from spline_module import Spline
-
-
-def calc_xy_curvature(s, sx, sy):
-    u"""
-    Calc curvature x-y spline curve
-    """
-    dx = sx.__calc__D(s)
-    ddx = sx.calcdd(s)
-    dy = sy.__calc__D(s)
-    ddy = sy.calcdd(s)
-    k = (ddy * dx - ddx * dy) / (dx ** 2 + dy ** 2)
-    return k
-
-
-def calc_yaw(s, sx, sy):
-    u"""
-    calc yaw of x-y spline curve
-    """
-    dx = sx.__calc__D(s)
-    dy = sy.__calc__D(s)
-    yaw = math.atan2(dy, dx)
-    return yaw
+from lagrange_module import LagrangePoly
 
 
 if __name__ == '__main__':
     # input
     #  x = [-1.0, 0.0, 1.0, 2.0, 3.0]
     #  x = [-0.5, 0.0, 0.5, 1.0, 1.5]
-    x = [1, 1.25, 1.5, 1.75, 2]
-    #  y = [3.2, 2.7, 6, 5, 6.5]
-    y = [1, 0.8, 0.6, 0.5, 0.5]
+    x = [1, 1.2, 1.5, 1.6, 1.8, 2]
+    y = [1, 0.81, 0.63, 0.61, 0.52, 0.5]
 
-    # 3d spline interporation
+    lg = LagrangePoly(x, y)
     spline = Spline(x, y)
-    rx = np.arange(1, 2, 0.01)
+    rx = np.arange(0, 2, 0.01)
     ry = [spline.calc(i) for i in rx]
 
     plt.plot(x, y, "xb", color='b')
-    plt.plot(rx, ry, "-r", color='r')
+    #plt.plot(rx, ry, "-r", color='r')
     plt.plot(x, np.divide(1, x), color='g')
+    plt.plot(x, lg.interpolate(x), color='m')
     plt.grid(True)
     plt.axis("equal")
     plt.show()
--- LR-7/spline_module.py
+++ LR-7/spline_module.py
@@ -3,13 +3,6 @@ import math
 
 
 class Spline:
-    u"""
-    Cubic Spline class
-    usage:
-        spline=Spline(x,y)
-        rx=np.arange(0,4,0.1)
-        ry=[spline.calc(i) for i in rx]
-    """
 
     def __init__(self, x, y):
         self.b, self.c, self.d, self.w = [], [], [], []
@@ -17,30 +10,21 @@ class Spline:
         self.x = x
         self.y = y
 
-        self.nx = len(x)  # dimension of x
+        self.nx = len(x)
         h = np.diff(x)
 
-        # calc coefficient c
         self.a = [iy for iy in y]
 
-        # calc coefficient c
         A = self.__calc__A(h)
         B = self.__calc__B(h)
         self.c = np.linalg.solve(A, B)
-        #  print(self.c1)
 
-        # calc spline coefficient b and d
         for i in range(self.nx - 1):
             self.d.append((self.c[i + 1] - self.c[i]) / (3.0 * h[i]))
             tb = (self.a[i + 1] - self.a[i]) / h[i] - h[i] * (self.c[i + 1] + 2.0 * self.c[i]) / 3.0
             self.b.append(tb)
 
     def calc(self, t):
-        u"""
-        Calc position
-        if t is outside of the input x, return None
-        """
-
         if t < self.x[0]:
             return None
         elif t > self.x[-1]:
@@ -53,18 +37,11 @@ class Spline:
         return result
 
     def __search_index(self, x):
-        u"""
-        search data segment index
-        """
-
         for i in range(self.nx):
             if self.x[i] - x > 0:
                 return i - 1
 
     def __calc__A(self, h):
-        u"""
-        calc matrix A for spline coefficient c
-        """
         A = np.zeros((self.nx, self.nx))
         A[0, 0] = 1.0
         for i in range(self.nx - 1):
@@ -80,9 +57,6 @@ class Spline:
         return A
 
     def __calc__B(self, h):
-        u"""
-        calc matrix B for spline coefficient c
-        """
         B = np.zeros(self.nx)
         for i in range(self.nx - 2):
             B[i + 1] = 3.0 * (self.a[i + 2] - self.a[i + 1]) / h[i + 1] - 3.0 * (self.a[i + 1] - self.a[i]) / h[i]
@@ -90,9 +64,6 @@ class Spline:
         return B
 
     def __calc__D(self, t):
-        u"""
-        Calc first derivative
-        """
 
         j = int(math.floor(t))
         if j < 0:
@@ -105,9 +76,6 @@ class Spline:
         return result
 
     def calcdd(self, t):
-        u"""
-        Calc second derivative
-        """
         j = int(math.floor(t))
         if j < 0:
             j = 0
