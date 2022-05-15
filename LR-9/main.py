1594144df61bef7cc4545b59dcc8e0f4b9185304
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2022-05-16 00:52:07 +0300

    LR-9

--- LR-9/main.py
+++ LR-9/main.py
@@ -1,3 +1,5 @@
+import time
+
 from euler_method_module import euler_method
 from runge_kutta_method_module import runge_kutta_method
 
@@ -5,6 +7,16 @@ from runge_kutta_method_module import runge_kutta_method
 def function(x, y):
     return 1.1*(1 - y**2)/(2*x**2 + y**2 + 1)
 
+print("Метод Эйлера:")
+t = time.perf_counter()
+euler = euler_method(function, 1000000, 0.000001, 0, 0)
+t_w = time.perf_counter() - t
+print(f"В точке x = {'%.6f' % euler[0]} имеет значение: {'%.6f' % euler[1]}")
+print(f"Время выполнения: {'%.6f' % t_w}")
 
-print(euler_method(function, 10000, 0.0001, 0, 0))
-print(runge_kutta_method(function, 10000, 0.0001, 0, 0))
+print("\nМетод Рунге-Кутта:")
+t = time.perf_counter()
+runge_kutta = runge_kutta_method(function, 1000000, 0.000001, 0, 0)
+t_w = time.perf_counter() - t
+print(f"В точке x = {'%.6f' % runge_kutta[0]} имеет значение: {'%.6f' % runge_kutta[1]}")
+print(f"Время выполнения: {'%.6f' % t_w}")
